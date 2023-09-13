816caceb2ee2d73c0799254db954c5b1fda90a9b
sagrikasrivastava <sagrikasrivastava94@gmail.com>
2023-09-13 21:00:54 +0400

    Committing the recent updates

--- src/main/java/org/sagsri/Main.java
+++ src/main/java/org/sagsri/Main.java
@@ -3,14 +3,14 @@ package org.sagsri;
 public class Main {
     public static void main(String[] args) {
         String Str = "Welcome";
-        String Str2[] = {"a", "e", "i", "o"};
+        String Str3[] = {"a", "e", "i", "o"};
         int count = 0;
-        for (int i= 0;i< Str2.length; i++){
-            if(Str.contains(Str2[i])){
+        for (int i= 0;i< Str3.length; i++){
+            if(Str.contains(Str3[i])){
                 System.out.println("Vowel found");
                 count++;
             } else {
-                System.out.println("No match found for Vowel " + Str2[i]);
+                System.out.println("No match found for Vowel " + Str3[i]);
             }
         }
         System.out.println("Total vowel count is " + count);
